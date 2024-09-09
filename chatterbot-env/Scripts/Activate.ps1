e711dbb1d99ab5b5b3afefb4117341e4ee088ded
joshuapagonas <65562873+joshuapagonas@users.noreply.github.com>
2024-09-08 23:16:40 -0400

    Second portion of the Project committed

---
+++ chatterbot-env/Scripts/Activate.ps1
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:80115fc7965bbfeb3d27adda25b3c531581eb38f29b646cb94a53c6bc5654a81
+size 26199
---
+++ chatterbot-env/Scripts/activate.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:530ecb14380d1792bd071a7f169f36f1753ddc9f748bdc8a038041ada1e47db3
+size 1058
